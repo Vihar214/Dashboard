927e60feea44a0c5c145751c9907acf9385dd35b
Vihar <viharpatel214@gmail.com>
2023-05-28 21:50:23 +0530

    Connected Userid and made userschema and added profile in both side bar and nave bar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -94,6 +94,7 @@ const navItems = [
 
 
 const Sidebar = ({
+    user,
     drawerWidth,
     isSidebarOpen,
     setIsSidebarOpen,
@@ -142,7 +143,7 @@ const Sidebar = ({
                 )}
               </FlexBetween>
             </Box>
-            <List>
+            <List sx={{bottom: "2rem"}}>
               {navItems.map(({ text, icon }) =>{
                 if(!icon){
                   return (
@@ -187,6 +188,38 @@ const Sidebar = ({
                 )
               })}
             </List>  
+          </Box>
+          <Box position="relative" bottom="2rem">
+              <Divider />
+              <FlexBetween textTransform="none" gap="1rem" m="1.5rem 2rem 0 3rem">
+                <Box
+                  component="img"
+                  alt="profile"
+                  src={profileImage}
+                  height="40px"
+                  width="40px"
+                  borderRadius="50%"
+                  sx={{ objectFit: "cover" }}
+                />
+                  <Box textAlign="left">
+                    <Typography 
+                      fontWeight="bold" 
+                      fontSize="0.9rem" 
+                      sx={{ color: theme.palette.secondary[100] }}
+                    >
+                      {user.name}
+                    </Typography>
+                    <Typography 
+                      fontSize="0.8rem" 
+                      sx={{ color: theme.palette.secondary[200] }}
+                    >
+                      {user.occupation} 
+                    </Typography>
+                  </Box>
+                  <SettingsOutlined
+                    sx={{ color: theme.palette.secondary[300],fontSize: "25px"}}
+                  />
+              </FlexBetween> 
           </Box> 
         </Drawer>
       )}
